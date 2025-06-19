35f558ec6bfb387004e3d8348b04347ce470a158
iliang0411 <126122790+iliang0411@users.noreply.github.com>
2025-06-19 08:35:28 -0400

    Update and rename max_avg_subarray.py to isPalindrome.py

---
+++ leetcode-ds-prep-flattened/python/arrays/isPalindrome.py
@@ -0,0 +1,10 @@
+# Palindrome
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        x_str = str(x)
+        return x_str == x_str[::-1]
